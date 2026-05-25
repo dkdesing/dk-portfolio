de6b79579b211ad494a29249806096c2ae5b101e
dkdesing <konskiyden@gmail.com>
2026-05-25 22:42:28 +0300

    portfolio update

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,11 @@
 import './style.css'
-export const metadata = { title: 'DK Design', description: 'Portfolio website' }
+export const metadata = { export const metadata = {
+  title: "DK Design",
+  description: "Interior / Exterior Design",
+  icons: {
+    icon: "/favicon.png",
+  },
+}
 export default function RootLayout({ children }) {
   return <html lang="uk"><body>{children}</body></html>
 }
